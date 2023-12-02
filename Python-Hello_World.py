daa84800f0ef971425842c60606a89fd9e4ae725
Delio Labaddan <149133144+DellPogie@users.noreply.github.com>
2023-12-02 14:29:20 +0800

    Add files via upload

---
+++ Python-Hello_World.py
@@ -0,0 +1,7 @@
+print("ooooo   ooooo           oooo  oooo                   oooooo   oooooo     oooo                    oooo        .o8  .o. ")
+print("`888'   `888'           `888  `888                    `888.    `888.     .8'                     `888        ""888  888 ")
+print(" 888     888   .ooooo.   888   888   .ooooo.           `888.   .8888.   .8'    .ooooo.  oooo d8b  888   .oooo888  888 ")
+print(" 888ooooo888  d88' `88b  888   888  d88' `88b           `888  .8'`888. .8'    d88' `88b `888""8P    888  d88' `888  Y8P ")
+print(" 888     888  888ooo888  888   888  888   888            `888.8'  `888.8'     888   888  888      888  888   888  `8' ")
+print(" 888     888  888    .o  888   888  888   888 .o.         `888'    `888'      888   888  888      888  888   888  .o. ")
+print("o888o   o888o `Y8bod8P' o888o o888o `Y8bod8P' Y8P          `8'      `8'       `Y8bod8P' d888b    o888o `Y8bod88P 'Y8P ")
